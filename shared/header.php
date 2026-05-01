08f8a72be076a7eee6e780b2a6d35aa7a072b9f4
Guasca-Dylan <dylan.guasca@cun.edu.co>
2026-05-01 15:13:51 -0500

    actualizaciones

--- shared/header.php
+++ shared/header.php
@@ -13,6 +13,7 @@ $usuario = getSession('usuario');
     <link rel="stylesheet" href="styles.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="icon" type="image/x-icon" href="images/ICONO.jpg">
 </head>
 
 <body>
